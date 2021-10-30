0b3d8c84f6a3877a5a3a0167b455e25eb5911312
Cristi <cristian.ciont@gmail.com>
2021-10-30 19:31:07 +0300

    Lab4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ciont_Cristian_Lab2.Data;
+using Ciont_Cristian_Lab2.Models.LibraryViewModels;
 
 namespace Ciont_Cristian_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +28,19 @@ namespace Ciont_Cristian_Lab2.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
